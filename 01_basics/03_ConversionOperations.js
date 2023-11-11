b258d48873df5cf88a2ed81bb3d3d5663d0cd3b7
Aakriti Mittal <aakritimt562@gmail.com>
2023-11-11 16:53:13 +0000

    conversion done

---
+++ 01_basics/03_ConversionOperations.js
@@ -0,0 +1,15 @@
+let score = "33abc"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueNumber = Number(score)
+console.log(typeof valueNumber);
+console.log(valueNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false =>0
+// null => object => 0
+//undefined => undefined => NaN
+// "" => faslse
